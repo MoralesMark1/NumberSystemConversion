210dcb0232bbe62dc15272785665a48ef749532f
Mark Morales <94729459+MoralesMark1@users.noreply.github.com>
2024-06-07 09:25:03 +0800

    Update numbersystemConvertion.py

--- numbersystemConvertion.py
+++ numbersystemConvertion.py
@@ -1,43 +1,34 @@
-isContinue = True
+baseNumber = {"OCTAL": 8, "HEXADECIMAL": 16, "BINARY": 2}
+values = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
 
-def ConvertDecimalValue(decimalValue):
-    baseNumber = {"OCTAL": 8, "HEXADECIMAL": 16, "BINARY": 2}
-    values = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
-    
-    
+def ConvertDecimalValue(decimalValue):    
     print(10*'#' + " Convertion Result " + '#'*11)
-    for currentBase in baseNumber:
-        
-        baseNum = baseNumber[currentBase]
-        
-        convertedValue = ""
-            
+    for currentBase in baseNumber:        
+        baseNum = baseNumber[currentBase]        
+        convertedValue = ""            
         currentValue = decimalValue
             
         while currentValue != 0:
             result = currentValue / baseNum
             fractionalValue = result % 1
-            #print(fractionalValue)
                 
             remainder = int(fractionalValue * baseNum)
             remainder = values[remainder]
             
-            convertedValue += str(remainder)
-                
+            convertedValue += str(remainder)                
             currentValue = int(result)
         
-        # Reverse the value to align the correct value
+        # Reverse the value to get the correct value
         convertedValue = convertedValue[::-1]
         
         print(f"| {currentBase:<13} | {convertedValue:>20} |")
-
-
+    print()
 
 while True:
     try:
         decimalValue = int(input("Input Decimal Number: "))        
         ConvertDecimalValue(decimalValue)
     except ValueError:
-      print("Invalid input. Please enter an integer only.")
+      print("Invalid input. Please enter an integer only.\n")
          
  
